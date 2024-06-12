f6ac364a1e36379bd5a48bdfc7954e91ed2bf922
Carlos Guisao Franco <carlos.guisao@globant.com>
2024-06-12 15:16:29 -0500

    Add overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,15 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1 Get tour data form collection
+  const tours = await Tour.find();
+
+  // 2 Build Template
+
+  // 3 Render that template using the data from 1
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker' });
