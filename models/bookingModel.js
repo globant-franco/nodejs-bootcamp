2e8ca04dc1720569768e7e2cac3eecca09d8404f
Carlos Franco <carlos.guisao@globant.com>
2024-06-19 08:46:55 -0500

    Add Booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    // in case someone pays in cash
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function () {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  });
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
